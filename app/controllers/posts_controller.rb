45d2d2d9ad118d36b8bf5a28fac911df19da4cf0
Keisuke Saotome <Keisuke@keisukes-MacBook-Air.local>
2017-01-07 18:58:20 +0900

    validation done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,39 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(posts_params)
+    if @post.save
+    redirect_to posts_path, notice: "ブログを更新しました"
+    else
+    render "new"
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(posts_params)
+    redirect_to posts_path
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+  def posts_params
+    params.require(:post).permit(:title, :content, :image)
   end
 end
--- db/migrate/20170107074446_create_posts.rb
+++ db/migrate/20170107074446_create_posts.rb
@@ -1,6 +1,9 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
+      t.string :title
+      t.text :content
+      t.binary :image
 
       t.timestamps null: false
     end
