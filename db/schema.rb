5d37badcd8c102eab3d7b46c3d323cbd658fa1bd
Keisuke Saotome <Keisuke@keisukes-MacBook-Air.local>
2017-01-17 20:31:12 +0900

    Fix migration file

--- db/schema.rb
+++ db/schema.rb
@@ -27,27 +27,27 @@ ActiveRecord::Schema.define(version: 20170115043407) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "name"
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
     t.string   "avatar"
-    t.string   "admin"
-    t.string   "uid",                    default: "", null: false
-    t.string   "provider",               default: "", null: false
+    t.boolean  "admin",                  default: false
+    t.string   "uid",                    default: "",    null: false
+    t.string   "provider",               default: "",    null: false
     t.string   "image_url"
   end
 
